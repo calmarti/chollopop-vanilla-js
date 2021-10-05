55931f5bde5542c7e179e26f476f7e0cee4989d6
Ivan Martinez <ivmartinezc@gmail.com>
2021-10-05 17:52:15 +0200

    activacion o no del boton mientras usuario escribe (validacion en tiempo real previa a submit)

--- js/controllers/SignUpController.js
+++ js/controllers/SignUpController.js
@@ -12,20 +12,24 @@ export default class SignUpController {
     }
 
     attachEventListeners() {
+
+
         this.element.addEventListener('submit', async event => {
             event.preventDefault()
 
             //hacer todas las validaciones necesarias
             if (this.element.checkValidity()) {
                 try {
-                   
-                    const username = this.element.querySelector('input[name="username"]').value
-                    const password = this.element.querySelector('input[name="password"]').value 
-                   /*  const controls = new FormData(this.element)
-                    const username = controls.get('username')
-                    const password = controls.get('password') */
-                    
-                    //console.log(username, password)
+                    const usernameElem = this.element.querySelector('input[name="username"]')
+                    const username = usernameElem.value
+                    const passwordElem = this.element.querySelector('input[name="password"]')
+                    const password = passwordElem.value
+
+                    /*  const controls = new FormData(this.element)
+                     const username = controls.get('username')
+                     const password = controls.get('password') */
+
+
                     const result = await DataService.registerUser(username, password)
                     console.log(result)
                     PubSub.publish(PubSub.events.SHOW_SUCCESS, 'Registrado con éxito')
@@ -38,20 +42,51 @@ export default class SignUpController {
             else {
                 console.log('THIS.ELEMENT', this.element)
                 console.log('THIS.ELEMENT.ELEMENTS', this.element.elements)
-                 let message = ``
-                 Array.from(this.element.elements).forEach(control => {
-                    if (control.validity.valid === false){
+                let message = ``
+                Array.from(this.element.elements).forEach(control => {
+                    if (control.validity.valid === false) {
                         message += `${control.name}: ${control.validationMessage}\n`    //¿porque ignora el newline?
                     }
                 })
                 PubSub.publish(PubSub.events.SHOW_ERROR, message)
             }
-                 
-           //proteccion contra HTML injection?
+
+            //proteccion contra HTML injection?
 
         })
+
+        //habilitación o no del botón mientras el usuario teclea (antes del evento 'submit')
+        const inputList = this.element.querySelectorAll('input')
+        const button = this.element.querySelector('button')
+        
+
+        Array.from(inputList).forEach(input => {
+            input.addEventListener('input', event => {
+                if (this.element.checkValidity()) {
+               
+                    button.removeAttribute('disabled')
+                }
+                else{
+                    button.setAttribute('disabled', true)
+                }
+            })
+        })
+        
+        //TODO: validar confirmacion de contraseña en tiempo real
+
+
+
+  
+
     }
 }
+
+
+
+
+
+
+
 
 
 
