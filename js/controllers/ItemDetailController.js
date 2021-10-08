52aedf030a17b78b627153e0f546ae83fd39c0d8
Ivan Martinez <ivmartinezc@gmail.com>
2021-10-08 21:06:28 +0200

    primera version de la barra de navegacion con enlaces a login y signup

--- js/controllers/ItemDetailController.js
+++ js/controllers/ItemDetailController.js
@@ -8,25 +8,28 @@ export default class ItemDetailController {
         this.element = element
         this.showItemDetail(itemId)
         console.log(itemId)
-        
+
 
     }
     //proteger del HTML injection
     async showItemDetail(itemId) {
         try {
             const item = await DataService.getItemDetail(itemId)
-            DataService.addCanBeDeletedProperty(item)       
+            DataService.addCanBeDeletedProperty(item)
             this.element.innerHTML = itemDetailView(item)
             const button = this.element.querySelector('button')
 
             //TODO: refactorizar en función aparte
-            if (button){
+            //TODO: mensaje de confirmación al usuario antes de borrar el anuncio
+
+            if (button) {
                 button.addEventListener('click', async () => {
-                        await DataService.deleteItem(itemId)
-                        window.location.href = '/?message=deleted-item'
-                    })
-                }
-                           
+                    await DataService.deleteItem(itemId)
+                    window.location.href = '/?message=deleted-item'
+                    button.setAttribute('disabled', 'disabled')
+                })
+            }
+
         }
         catch (error) {
             PubSub.publish(PubSub.events.SHOW_ERROR, error)
@@ -34,8 +37,8 @@ export default class ItemDetailController {
 
     }
 
-  
 
-} 
+
+}
 
 
